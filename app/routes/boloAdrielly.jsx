a32992ff303947c921526cea1e8c38e6af4bc6e3
ad132p <neto.acs@gmail.com>
2023-07-13 01:40:14 -0300

    added sisten in law's cake route

---
+++ app/routes/boloAdrielly.jsx
@@ -0,0 +1,9 @@
+import bolo from "../images/bolo.jpeg"
+
+export default function BoloAdrielly () {
+    return <>
+    <div className="grid place-items-center">
+        <img src={bolo} width="600" height="auto"></img>
+    </div>
+    </>
+}
